54a6f5a18dfb7e92d37c2c2affb7b4580f296d77
unknown0106 <jingjingz.simp@gmail.com>
2026-01-06 16:15:35 +0800

    Create eval_aime24_aime25_math500.py
    Signed-off-by: unknown0106 <jingjingz.simp@gmail.com>

---
+++ eval_aime24_aime25_math500.py
@@ -0,0 +1,10 @@
+from evalscope import TaskConfig, run_task
+task_config = TaskConfig(
+    api_url='http://0.0.0.0:8802/v1/chat/completions',  # 推理服务地址
+    model='qwen2.5-14b-instruct',  
+    eval_type='openai_api',  
+    datasets=['aime24', 'aime25', 'math_500'], 
+    dataset_args={'math_500': {'few_shot_num': 0, 'subset_list': ['Level 1', 'Level 2', 'Level 3', 'Level 4', 'Level 5']}},
+    eval_batch_size=32
+)
+run_task(task_config)
